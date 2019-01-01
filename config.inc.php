1f26a909486924db5e05d90d9a409261b0df6003
Fabrice Pipart <fabrice.pipart@gmail.com>
2019-01-01 13:41:15 +0100

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'maraidb';
+$cfg['Servers'][$i]['host'] = 'mariadb';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
